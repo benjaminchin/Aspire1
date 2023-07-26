235a462aa83e006430f9792f52a0904241230231
benjaminchin <benjaminchin@vt.edu>
2023-07-26 16:35:57 -0400

    init commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // URL format: /[Controller]/[ActionName]/[Parameters]
+        // GET: /HelloWorld/Welcome
+        public IActionResult Welcome(string name, int numTimes=1)
+        {
+            ViewData["message"] = "Hello, " + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
